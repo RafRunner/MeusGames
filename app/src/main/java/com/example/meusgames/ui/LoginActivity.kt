8ae15ccf30282d8f398fa5639c10697440f8dc4e
Rafael Santana <rafaelns.br@gmail.com>
2021-01-29 20:59:27 -0300

    Leambrando da opção do remember me

--- app/src/main/java/com/example/meusgames/ui/LoginActivity.kt
+++ app/src/main/java/com/example/meusgames/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.meusgames.ui
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,6 +14,15 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bind = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(bind.root)
+
+        val prefs = getSharedPreferences("login", Context.MODE_PRIVATE)
+        val rememberMe = prefs.getBoolean("remember_me", true)
+
+        bind.checkRemember.isChecked = rememberMe
+
+        bind.checkRemember.setOnCheckedChangeListener { _, isChecked ->
+            prefs.edit().putBoolean("remember_me", isChecked).apply()
+        }
 
         bind.btnLogin.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
